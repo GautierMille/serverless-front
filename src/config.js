35992c6d6617a8fa6d99bcae14ea010d2f6d2633
Gautier Mille <gautier.mille@gmail.com>
2020-02-11 15:23:41 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-uploads-gautier"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://czdju728p3.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_RZna78g3b",
+        APP_CLIENT_ID: "23tuovq3bs4s1p3clptr8rmbks",
+        IDENTITY_POOL_ID: "us-east-2:992b871a-f3d9-4fa6-ba41-8b17cf35b013"
+    }
+};
